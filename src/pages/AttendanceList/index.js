77407f430b5770fd973fb28fecc062d39737ce78
FMGordillo <facundomgordillo@gmail.com>
2019-04-18 20:10:06 -0300

    Added "loading" state

--- src/pages/AttendanceList/index.js
+++ src/pages/AttendanceList/index.js
@@ -1,45 +1,47 @@
-import React, { useState, useEffect } from "react"
-import { GITHUB_ACCOUNTS } from "../../utils/constants"
-import { getAllAssitance } from "../../utils/api"
+import React, { useState, useEffect } from "react";
+import { GITHUB_ACCOUNTS } from "../../utils/constants";
+import { getAllAssitance } from "../../utils/api";
 
-import './index.css'
+import "./index.css";
 
 const ListOfAttendance = () => {
-  const [alumns, setAlumns] = useState({})
+  const [alumns, setAlumns] = useState({});
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    async function fetchData() {
-      const { data } = await getAllAssitance()
+    fetchData().then(() => setLoading(false));
+    return () => {};
+  });
 
-      if (Object.keys(alumns).length <= 0 && data.response.docs.length > 0) {
-        const depuratedAlumns = data.response.docs.reduce(
-          (acc, curr) => {
-            var key = curr["date"]
-            if (!acc[key]) {
-              acc[key] = []
-            }
-            acc[key].push(curr)
-            return acc
-          },
-          []
-        )
-        setAlumns(depuratedAlumns)
-      }
+  async function fetchData() {
+    const { data } = await getAllAssitance();
+    const { docs } = data.response;
+
+    if (Object.keys(alumns).length <= 0 && docs.length > 0) {
+      const depuratedAlumns = docs.reduce((acc, curr) => {
+        var key = curr["date"];
+        if (!acc[key]) {
+          acc[key] = [];
+        }
+        acc[key].push(curr);
+        return acc;
+      }, []);
+      setAlumns(depuratedAlumns);
     }
-    fetchData()
-    return () => {
-    }
-  })
+  }
 
-  console.log(alumns)
-
-  return (
+  return loading ? (
+    <div className="loading">
+      <span>Loading, please wait...</span>
+    </div>
+  ) : (
     <table>
       <thead>
         <tr>
           <th>Nombre de usuario</th>
 
           {Object.keys(alumns).map((i, k) => {
-            return <th key={k}>{i}</th>
+            return <th key={k}>{i}</th>;
           })}
         </tr>
       </thead>
@@ -48,14 +50,16 @@ const ListOfAttendance = () => {
           <tr key={i}>
             <td>{account}</td>
             {Object.keys(alumns).map((date, i) => {
-              const line = alumns[date].find(el => el.user === account)
-              return <td key={i}>{line ? (line.isPresente && "✅" || "🍺") : ""}</td>
+              const line = alumns[date].find(el => el.user === account);
+              return (
+                <td key={i}>{line ? (line.isPresente && "✅") || "🍺" : ""}</td>
+              );
             })}
           </tr>
         ))}
       </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default ListOfAttendance
+export default ListOfAttendance;
